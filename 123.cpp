f2deee0bacd7ab4c94a6d0ba4634a7996867f16a
911050151 <911050151@qq.com>
2015-11-18 16:08:55 +0800

    123

--- 123.cpp
+++ 123.cpp
@@ -1,17 +1,54 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-using namespace cv;
-int main()
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <cv.h>
+#include <highgui.h> 
+ 
+int main(int argc, char *argv[])
 {
-	Mat img1=imread("12.jpg");
-	Mat img2=imread("13.jpg");//俩张图片的大小要一样
-	imshow("1",img1);
-	imshow("2",img2);
-	Mat img_result1,img_result2,img_result;
-	subtract(img1,img2,img_result1);
-	subtract(img2,img1,img_result2);
-	add(img_result1,img_result2,img_result1);
-	imshow("result",img_result1);
-	waitKey();
-	return 0;
+  IplImage* img = 0; 
+  int height,width,step,channels;
+  uchar *data;
+  int i,j,k; 
+ 
+  if(argc<2){
+    printf("Usage: main <image-file-name>\n\7");
+   // exit(0);
+  } 
+ 
+  // load an image  
+  img=cvLoadImage("1.jpg",1);
+  if(!img){
+    printf("Could not load image file: %s\n",argv[1]);
+    //exit(0);
+  } 
+ 
+  // get the image data
+  height    = img->height;
+  width     = img->width;
+  step      = img->widthStep;
+  channels  = img->nChannels;
+  data      = (uchar *)img->imageData;
+  printf("Processing a %dx%d image with %d channels\n",height,width,channels); 
+ 
+  // create a window
+  cvNamedWindow("mainWin", CV_WINDOW_AUTOSIZE); 
+  cvMoveWindow("mainWin", 100, 100); 
+ 
+  // invert the image
+  // 相当于 cvNot(img);
+  // IplImage *pDstImg = cvCreateImage(cvGetSize(img),img->depth,img->nChannels);
+  // cvNot(img, pDstImg);
+  for(i=0;i<height;i++) for(j=0;j<width;j++) for(k=0;k<channels;k++)
+    data[i*step+j*channels+k]=255-data[i*step+j*channels+k]; 
+ 
+  // show the image
+  cvShowImage("mainWin", img ); 
+ 
+  // wait for a key
+  cvWaitKey(0); 
+ 
+  // release the image
+  cvReleaseImage(&img );
+  return 0;
 }
